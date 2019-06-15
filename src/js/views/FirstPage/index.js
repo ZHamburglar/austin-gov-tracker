2e32e8207c520004730949fbe9c43370e3356a0a
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2019-06-15 01:05:45 -0500

    webpack style compilers

--- src/js/views/FirstPage/index.js
+++ src/js/views/FirstPage/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 
+import styles from './style.css';
 // Let me tell you about the other guys
+
 class FirstPage extends Component {
   render() {
     return (
-      <div>First Page here<div>boooooo</div></div>
+      <div>First Page here<div className={styles.localcolor}>boooooo</div></div>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const combineLoaders = require('webpack-combine-loaders');
+
 // const SystemBellPlugin = require('system-bell-webpack-plugin');
 
 module.exports = {
@@ -37,15 +40,45 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          { 
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            }
+          },
+        ],
       },
+      // {
+      //   test: /\.css$/,
+      //   loader: 'style-loader'
+      // }, 
+      // {
+      //   test: /\.css$/,
+      //   loader: 'css-loader',
+      //   options: {
+      //     importLoaders: 2,
+      //     modules: true,
+      //     camelCase: true,
+      //     // localIdentName: '[path][name]__[local]--[hash:base64:5]',
+      //   },
+      // }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
     // new SystemBellPlugin(),
   ]
