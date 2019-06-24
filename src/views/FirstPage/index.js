e2b73acc59f2a9f82af32e7b9cd133c051ffb725
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2019-06-24 18:23:44 -0500

    eslint and prettier configurations created

--- src/views/FirstPage/index.js
+++ src/views/FirstPage/index.js
@@ -1,26 +1,39 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { func, number } from 'prop-types';
-import { addFirstTotal, subFirstTotal, addSecondTotal, subSecondTotal } from '@modules/Calculator/actions';
+import {
+  addFirstTotal,
+  subFirstTotal,
+  addSecondTotal,
+  subSecondTotal,
+} from '@modules/Calculator/actions';
 import { getFirstTotal, getSecondTotal } from '@modules/Calculator/selectors';
 
 import styles from './style.scss';
 // Let me tell you about the other guys
 
 class FirstPage extends Component {
+  state = {
+    firstValue: 0,
+  };
+
   render() {
     const { first_total, second_total } = this.props;
+    const { firstValue } = this.state;
     return (
       <div>
         <p>First Page here</p>
         <div className={styles.localcolor}>This is local text.</div>
-        <div className="global-text">This is the global text</div>
+        <div className="global-text">This is the global text.</div>
+        <div className="global-text">{firstValue}</div>
         <div className="pb-2">
           <div className="d-inline-block">
             <button onClick={() => this.props.addFirstTotal()}>Add +</button>
           </div>
-          <div className="d-inline-block px-2 text-hover-red">First Total: {first_total}</div>
+          <div className="d-inline-block px-2 text-hover-red">
+            First Total: {first_total}
+          </div>
           <div className="d-inline-block">
             <button onClick={() => this.props.subFirstTotal()}>Sub -</button>
           </div>
@@ -29,13 +42,13 @@ class FirstPage extends Component {
           <div className="d-inline-block">
             <button onClick={() => this.props.addSecondTotal()}>Add +</button>
           </div>
-          <div className="d-inline-block px-2 text-hover-blue">Second Total: {second_total}</div>
+          <div className="d-inline-block px-2 text-hover-blue">
+            Second Total: {second_total}
+          </div>
           <div className="d-inline-block">
             <button onClick={() => this.props.subSecondTotal()}>Sub -</button>
           </div>
         </div>
-        
-        
       </div>
     );
   }
@@ -65,11 +78,11 @@ const mapStateToProps = createStructuredSelector({
  * Export the component
  */
 export default connect(
-  mapStateToProps, 
-  { 
-    addFirstTotal, 
-    subFirstTotal, 
-    addSecondTotal, 
-    subSecondTotal
+  mapStateToProps,
+  {
+    addFirstTotal,
+    subFirstTotal,
+    addSecondTotal,
+    subSecondTotal,
   }
 )(FirstPage);
